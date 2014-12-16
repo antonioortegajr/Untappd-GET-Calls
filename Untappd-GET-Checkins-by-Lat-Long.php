b798e9cef94bf6ec7536ceab5d0af1dbb9036e07
antonioortegajr <antonioortega@8808.local>
2014-12-15 18:59:19 -0800

    initial commit
    Get call by lat and long for checkins

---
+++ Untappd-GET-Checkins-by-Lat-Long.php
@@ -0,0 +1,32 @@
+<?php
+
+//You will need an API key and secret from Untapped
+//simple call to get untappd check ins from the area specified by lat and log
+
+$gate = 'http://api.untappd.com/v4/thepub/local?limit=50';
+
+$lat = 'YourLat';
+
+$lng = 'YourLong';
+
+
+$client_id = 'YourID';
+
+$client_secret = 'YourSecret';
+
+
+
+$ch = curl_init($gate . '&lat=' . $lat . '&lng=' . $lng . '&client_id=' . $client_id . '&client_secret=' . $client_secret);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+
+$data = curl_exec($ch);
+
+curl_close($ch);
+
+
+// dump out the data returned
+var_dump($data);
+?>
